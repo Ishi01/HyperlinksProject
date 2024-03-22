f3fb1477ef0224ac58e290d073038b305109a437
Ishi01 <60782549+Ishi01@users.noreply.github.com>
2024-03-22 10:15:59 +0800

    Initialisation of the repo
    Adding the already created files to a new repo

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+function goToHistory(){
+    location.href = "historypage.html";
+}
+
+function goToHome(){
+    location.href = "homepage.html";
+}
+
+function goToAbout(){
+    location.href = "abouttheauthor.html";
+}
