ffcbeceafb0a060120ed4b8805aefb55fec0406c
Ishi01 <60782549+Ishi01@users.noreply.github.com>
2024-04-10 13:55:43 +0800

    Added a bunch of text to the history page, the picture of tim berners-lee and the dynamic hyperlink demo Fix dynamic link bug and update CSS styling

--- javascript.js
+++ javascript.js
@@ -10,3 +10,16 @@ function goToHome(){
 function goToAbout(){
     location.href = "abouttheauthor.html";
 }
+
+function dynamicLinkExample(){
+    /*Dynamic Link Example*/
+    /*This solution isn't optimal but its a fix to weird bug where pressing the button would swap the checked radio button for seemingly no reason */
+    option1State = document.getElementById("linkOption1").checked;
+    option2State = document.getElementById("linkOption2").checked;
+    if (option1State == true){
+        location.href = "homepage.html";
+    }
+    if (option2State == true){
+        location.href = "abouttheauthor.html";
+    }
+}
